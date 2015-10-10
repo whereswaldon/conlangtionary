1b9f90790ed82d1cb7221d227fbab7353f3ce584
Chris Waldon <christopher.waldon.dev@gmail.com>
2015-10-10 17:36:54 -0400

    Add roles to User model. Ensure bidirectional migration.

---
+++ database/migrations/2015_10_10_213039_add_roles_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRolesToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table) {
+            $table->integer('role_id')->unsigned()->default(Role::where('name', 'User')->first()->id);
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table) {
+            $table->dropForeign('users_role_id_foreign');
+            $table->dropColumn('role_id');
+        });
+    }
+}
