a077e2f7f93089bead45569171314aaf26d8e270
Chris Waldon <christopher.waldon.dev@gmail.com>
2015-11-04 13:31:59 -0500

    Define migration to create tags table and the pivot between it and the definitions table.

---
+++ database/migrations/2015_11_04_040741_create_tags_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('language_id')->unsigned();
+            $table->foreign('language_id')->references('id')->on('languages');
+            $table->softDeletes();
+            $table->nullableTimestamps();
+        });
+
+        Schema::create('definition_tag', function(Blueprint $table) {
+            $table->integer('definition_id')->unsigned()->index();
+            $table->foreign('definition_id')
+                ->references('id')
+                ->on('definitions')
+                ->onDelete('cascade');
+            $table->integer('tag_id')->unsigned()->index();
+            $table->foreign('tag_id')
+                ->references('id')
+                ->on('tags')
+                ->onDelete('cascade');
+            $table->nullableTimestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('definition_tag');
+        Schema::drop('tags');
+    }
+}
