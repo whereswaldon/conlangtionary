cd6e7584c5f7501b892976ad68d3f72e53af2031
Chris Waldon <christopher.waldon.dev@gmail.com>
2015-11-19 23:30:01 -0500

    Check for tags set in request before iterating over them

--- app/Http/Controllers/DefinitionsController.php
+++ app/Http/Controllers/DefinitionsController.php
@@ -150,19 +150,21 @@ class DefinitionsController extends Controller
 
         //Attach the given tags to the word.
         $languageTags = $definition->word->language->tags; //Get all tags on the language
-        foreach($data['tags'] as $tagName) { //attach each tag
-            $matchingTag = $languageTags->where('name', $tagName)->first();
-            if ( $matchingTag ) { //if the tag exists, just associate it.
-                if (! $definition->tags->contains($matchingTag)) { //if the tag is already on the word
-                    $definition->tags()->attach($matchingTag->id);
+        if (isset($data['tags'])) {
+            foreach($data['tags'] as $tagName) { //attach each tag
+                $matchingTag = $languageTags->where('name', $tagName)->first();
+                if ( $matchingTag ) { //if the tag exists, just associate it.
+                    if (! $definition->tags->contains($matchingTag)) { //if the tag is already on the word
+                        $definition->tags()->attach($matchingTag->id);
+                    }
+                } else { //otherwise, create it and then associate it
+                    $newTag = $definition->word->language->tags()->create([
+                        'name' => $tagName,
+                        'abbreviation' => "($tagName)",
+                        'description' => 'A new tag, as yet undefined',
+                    ]);
+                    $definition->tags()->attach($newTag);
                 }
-            } else { //otherwise, create it and then associate it
-                $newTag = $definition->word->language->tags()->create([
-                    'name' => $tagName,
-                    'abbreviation' => "($tagName)",
-                    'description' => 'A new tag, as yet undefined',
-                ]);
-                $definition->tags()->attach($newTag);
             }
         }
         return redirect()->action('LanguagesController@show', [$definition->word->language->id]);
